df5529468eb33f97cd72d85b8d8e37148f29086e
NekoChan <aaron74327@gmail.com>
2024-09-13 00:12:16 -0700

    Update tailwind.config.ts

--- csr-basic-auth/tailwind.config.ts
+++ csr-basic-auth/tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'tailwindcss'
 export default {
   content: [
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.tsx',
   ],
   theme: {
     extend: {},
